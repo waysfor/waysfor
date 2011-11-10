7b598da62ef9cf485c06cc037a9291adfe4dfbe8
Lisces <lisces@gmail.com>
2011-11-10 20:00:06 +0800

    normal commit

--- app/controllers/manage.php
+++ app/controllers/manage.php
@@ -1,6 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Manage extends CI_Controller {
+  public $nav;
   public $act;
   public $userid;
   public $username;
@@ -19,13 +20,24 @@ class Manage extends CI_Controller {
         exit;
       }
     }
+    //生成导航菜单
+    $this->_nav();
+  }
+
+  private function _nav() {
+    $this->nav = $this->load->view('manage/nav.html', '', true);
   }
 
   function index() { //后台管理默认页面
     $out = array();
-    $out['user']['id'] = $this->userid;
+    $out['user']['id']   = $this->userid;
     $out['user']['name'] = $this->username;
     $out['user']['real'] = $this->realname;
+    $usercookie  = $this->input->cookie('user');
+    $usercookie  = json_decode($usercookie, true);
+    $out['user'] = array_merge($out['user'], $usercookie);
+
+    $out['nav'] = $this->nav;
     $this->load->view('manage/header.html');
     $this->load->view('manage/index.html', $out);
     $this->load->view('manage/footer.html');
@@ -37,16 +49,61 @@ class Manage extends CI_Controller {
       $password = $this->input->post('password');
       $this->load->model('usermodel');
       $user = $this->usermodel->login($username, $password);
-      print_r($user);exit;
-      if(empty($user)) { //登录失败
-        
+      $role = isset($user['role']) ? $user['role'] : 0;
+      $res = array();
+      $res['stat'] = 0;
+      $res['data'] = array();
+      if($role <= 0) { //登录失败
+        $res['stat'] = 1;
       } else { //登录成功
-        
+        //userid username realname写入到session
+        $usersess = array();
+        $usersess['id'] = $user['id'];
+        $usersess['name'] = $user['username'];
+        $usersess['real'] = $user['realname'];
+        $this->session->set_userdata(array('user' => $usersess));
+        //role addtm lasttm lastip 写入到cookie
+        $usercookie = array();
+        $rolearray = array(
+          0 => '普通用户',
+          1 => '初级管理员',
+          2 => '高级管理员',
+          3 => '超级管理员'
+        );
+        $usercookie['role'] = $rolearray[$role];
+        $usercookie['addtm'] = date('Y-m-d H:i:s', $user['addtm']);
+        $usercookie['lasttm'] = date('Y-m-d H:i:s', $user['lasttm']);
+        $usercookie['lastip'] = long2ip($user['lastip']);
+        $usercookie = json_encode($usercookie);
+        $cookie = array(
+          'name'   => 'user',
+          'value'  => $usercookie,
+          'expire' => 60*60*24*1000 //1000天过期时间
+        );
+        $this->input->set_cookie($cookie);
+        $res['data'] = $user;
       }
+      echo json_encode($res);
+      exit;
     } else { //page
       $this->load->view('manage/header-login.html');
       $this->load->view('manage/login.html');
       $this->load->view('manage/footer-login.html');
     }
+  }
+
+  function logout() {
+    //清理session
+    $this->session->unset_userdata('user');
+    //清理cookie
+    $cookie = array(
+      'name'   => 'user',
+      'value'  => '',
+      'expire' => ''
+    );
+    $this->input->set_cookie($cookie);
+    //跳转到登录页
+    echo '<script>window.location.href="/manage/login";</script>';
+    exit;
   }
 }
--- www/www/manage/js/custom.js
+++ www/www/manage/js/custom.js
@@ -1,48 +1,40 @@
-// 1 - START DROPDOWN SLIDER SCRIPTS ------------------------------------------------------------------------
-
+// 1 - START DROPDOWN SLIDER SCRIPTS
 $(document).ready(function () {
-    $(".showhide-account").click(function () {
-        $(".account-content").slideToggle("fast");
-        $(this).toggleClass("active");
-        return false;
-    });
+  $(".showhide-account").click(function () {
+    $(".account-content").slideToggle("fast");
+    $(this).toggleClass("active");
+    return false;
+  });
 });
 
 $(document).ready(function () {
-    $(".action-slider").click(function () {
-        $("#actions-box-slider").slideToggle("fast");
-        $(this).toggleClass("activated");
-        return false;
-    });
+  $(".action-slider").click(function () {
+    $("#actions-box-slider").slideToggle("fast");
+    $(this).toggleClass("activated");
+    return false;
+  });
 });
-
 //  END ----------------------------- 1
 
-// 2 - START LOGIN PAGE SHOW HIDE BETWEEN LOGIN AND FORGOT PASSWORD BOXES--------------------------------------
-
+// 2 - START LOGIN PAGE SHOW HIDE BETWEEN LOGIN AND FORGOT PASSWORD BOXES
 $(document).ready(function () {
-	$(".forgot-pwd").click(function () {
-	$("#loginbox").hide();
-	$("#forgotbox").show();
-	return false;
-	});
-
+  $(".forgot-pwd").click(function () {
+    $("#loginbox").hide();
+    $("#forgotbox").show();
+    return false;
+  });
 });
 
 $(document).ready(function () {
-	$(".back-login").click(function () {
-	$("#loginbox").show();
-	$("#forgotbox").hide();
-	return false;
-	});
+  $(".back-login").click(function () {
+    $("#loginbox").show();
+    $("#forgotbox").hide();
+    return false;
+  });
 });
-
 // END ----------------------------- 2
 
-
-
-// 3 - MESSAGE BOX FADING SCRIPTS ---------------------------------------------------------------------
-
+// 3 - MESSAGE BOX FADING SCRIPTS
 $(document).ready(function() {
 	$(".close-yellow").click(function () {
 		$("#message-yellow").fadeOut("slow");
@@ -57,42 +49,8 @@ $(document).ready(function() {
 		$("#message-green").fadeOut("slow");
 	});
 });
-
 // END ----------------------------- 3
-
-
-
-// 4 - CLOSE OPEN SLIDERS BY CLICKING ELSEWHERE ON PAGE -------------------------------------------------------------------------
-
-$(document).bind("click", function (e) {
-    if (e.target.id != $(".showhide-account").attr("class")) $(".account-content").slideUp();
-});
-
-$(document).bind("click", function (e) {
-    if (e.target.id != $(".action-slider").attr("class")) $("#actions-box-slider").slideUp();
-});
-// END ----------------------------- 4
  
- 
- 
-// 5 - TABLE ROW BACKGROUND COLOR CHANGES ON ROLLOVER -----------------------------------------------------------------------
-/*
-$(document).ready(function () {
-    $('#product-table	tr').hover(function () {
-        $(this).addClass('activity-blue');
-    },
-    function () {
-        $(this).removeClass('activity-blue');
-    });
-});
- */
-// END -----------------------------  5
- 
- 
- 
- // 6 - DYNAMIC YEAR STAMP FOR FOOTER -----------------------------------------------------------------------
-
- $('#spanYear').html(new Date().getFullYear()); 
- 
-// END -----------------------------  6 
-  
+// 6 - DYNAMIC YEAR STAMP FOR FOOTER
+$('#spanYear').html(new Date().getFullYear());
+// END ----------------------------- 6
