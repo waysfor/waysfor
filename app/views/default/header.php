0286b9451f3b49322073f4ad978c669cc15e4022
waysfor <zing02223@163.com>
2011-11-28 16:21:37 +0800

    'wfg'

--- app/views/default/header.php
+++ app/views/default/header.php
@@ -1,9 +1,39 @@
 <!DOCTYPE html>
-<html> 
-<head> 
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="keywords" content="第一号网站" /> 
-	<meta name="description" content="111" /> 
-	<title>第一号网站</title>
-</head>
-<body>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>首页</title>
+        <link rel="stylesheet" type="text/css" href="/www/default/css/index.css" />
+        <script type="text/javascript" src="/www/default/js/jquery.js"></script>
+        <script type="text/javascript" src="/www/default/js/base.js"></script>
+    </head>
+    <body>
+        <div class="header">
+            <div class="header_wrap">
+                <h1 class="logo">
+                    <a href="#">上海聚宇企业管理咨询</a>
+                </h1>
+                <div class="nav clear">
+                    <a href="#" class="current"><span>首页</span></a>
+                    <a href="#"><span>公开课</span></a>
+                    <a href="#"><span>企业内训</span></a>
+                    <a href="#"><span>培训讲师</span></a>
+                    <a href="#"><span>培训资讯</span></a>
+                    <a href="#"><span>培训需求</span></a>
+                    <a href="#"><span>课程搜索</span></a>
+                </div>
+                <form action="">
+                    <div class="search">
+                        <span class="search_type" id="search_type"><em id="option">公公开课开课</em><i class="arrow">arrow</i></span><input type="text" class="txt" /><input type="submit" class="btn" value="搜索" />
+                        <input type="hidden" value="公开课" id="checked" />
+                        <ul class="typelist" id="typelist">
+                            <li><a href="#">公开课</a></li>
+                            <li><a href="#">企业内训</a></li>
+                            <li><a href="#">培训讲师</a></li>
+                            <li><a href="#">培训资讯</a></li>
+                            <li><a href="#">培训需求</a></li>
+                        </ul>
+                    </div>
+                </form>
+            </div>
+        </div>
