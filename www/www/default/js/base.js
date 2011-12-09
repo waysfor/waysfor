8ad2be6b110f33150fe7ea3a6db8183e6ef7b739
waysfor <517020375@qq.com>
2011-12-09 11:58:19 +0800

    ...

--- www/www/default/js/base.js
+++ www/www/default/js/base.js
@@ -1,18 +1,48 @@
-$(document).ready(function(){
-    var typebtn = $("#search_type")
-    var typelist = $("#typelist")
-    var checked = $("#checked")
-    var option = $("#option")
-    var typeitem = $("#typelist li")
-    typebtn.click(function(event){
-        typelist.slideDown('fast')
-        event.stopPropagation()
-    })
-    $("body").click(function(){
-        typelist.slideUp('fast')
-    })
-    typeitem.click(function(){
-        option.text($(this).text())
-        checked.val($(this).text())
-    })
-})
+if (window.attachEvent){
+	window.attachEvent("onload",init);
+}
+else{
+	window.addEventListener("load",init,false);
+}
+
+function $(str){
+	if (str.indexOf("#") != -1){
+		return document.getElementById(str.substring(1));
+	}
+	else if(str.indexOf("#") == -1 && str.indexOf(".") == -1){
+		return document.getElementsByTagName(str);
+	}
+	else if(str.indexOf(".") != -1){
+		var aTemp = [];
+		var o = document.getElementsByTagName("*");
+		for (var i = 0; i < o.length; i++){
+			if (o[i].className.indexOf(str.substring(1)) != -1){
+				aTemp.push(o[i]);
+			}
+		}
+		return aTemp
+	}
+}
+
+function init(){
+	var typebtn = $("#search_type");
+	var typelist = $("#typelist");
+	var checked = $("#checked");
+	var option = $("#option");
+	var typeitem = typelist.getElementsByTagName("li");
+	var oBody = $("body")[0];
+	typebtn.onclick = function(ev){
+		oEvent = ev || window.event;
+		typelist.style.display = "block";
+		oEvent.cancelBubble = true;
+	}
+	oBody.onclick = function(){
+		typelist.style.display = "none";
+	}
+	for (var i = 0; i < typeitem.length; i++){
+		typeitem[i].onclick = function(){
+			option.innerHTML = this.innerHTML;
+			checked.value = this.innerHTML;
+		}
+	}
+}
