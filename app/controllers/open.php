fcee362bd22545dfd00e54920b6c8e8e2e4846a4
waysfor <zing02223@163.com>
2011-12-09 18:12:43 +0800

    'update.list.111209.002.zing'

--- app/controllers/open.php
+++ app/controllers/open.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Main extends CI_Controller {
+class Open extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -19,24 +19,16 @@ class Main extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this -> load -> model('NewsModel');
-		$class['news'] = $this -> NewsModel -> news_index('news');
-		
 		$this -> load -> model('OpenModel');
 		$class['now'] = $this -> OpenModel -> new_open('history','1');
 		$class['recommend'] = $this -> OpenModel -> recommend_open('history','1');
-		
-		$this -> load -> model('TrainModel');
-		$class['train'] = $this -> TrainModel -> train_index('history','2');
-		
-		$this -> load -> model('TrainerModel');
-		$class['trainer'] = $this -> TrainerModel -> trainer_index('trainer');
+		$class['old'] = $this -> OpenModel -> old_open('history','1');
 		
 		$this -> load -> model('CateModel');
 		$class['cate'] = $this -> CateModel -> cate('cate');
 		
 		$this->load->view('default/header');
-		$this->load->view('default/index',$class);
+		$this->load->view('default/list/openlist',$class);
 		$this->load->view('default/footer');
 	}
 }
