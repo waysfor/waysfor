3deda6f6d5b1b3e1e108ac0be5f69f63e33c2f26
waysfor <zing02223@163.com>
2012-01-31 23:55:51 +0800

    'add.linkme.php'

---
+++ app/views/default/linkme/linkme.php
@@ -0,0 +1,100 @@
+        <div class="wrapper">
+            <div class="content">
+            	<div class="row col2">
+                    <div class="rt">
+                        <h2>
+                            <span><a href="/about">联系我们</a></span>
+                        </h2>
+                    </div>
+                    <div class="rc clearfix">
+                        <div class="cr">
+							<div class="box">
+                                <div class="bt">
+                                    <h3>
+                                        <span>联系我们</span>
+                                    </h3>
+                                </div>
+                                <div class="bc">
+                                    <dl class="list">
+                                        <dt class="cerrent">联系方式</dt>
+										<dd>公司地址</dd>
+										<dd>咨询电话</dd>
+										<dd>联系邮箱</dd>
+                                        <dt>在线QQ客服</dt>
+										<dd>客服热线1：公司地址</dd>
+										<dd>客服热线2：公司电话</dd>
+										<dd>客服热线3：服务团队</dd>
+                                        <dt>在线MSN客服</dt>
+										<dd>客服热线4：</dd>
+										<dd>客服热线5：</dd>
+										<dd>客服热线6：</dd>
+                                    </dl>
+                                </div>
+							</div>
+                        </div>
+                        <div class="cl">
+							<div class="box">
+								<div class="bt">
+									<h3>
+										<span>联系我们</span>
+									</h3>
+								</div>
+								<div class="bc">
+									<div class="art">
+										<h3>公司简介</h3>
+										<img src="/www/default/img/image/company_top.jpg" alt="" />
+										<p>上海聚宇企业管理咨询有限公司，是一家专业的综合性培训公司，以教练式帮助解决企业实际问题的新型管理咨询培训机构。业务覆盖综合管理、人事、生产，职业技能及销售等领域。突破传统培训咨询类公司单一运作模式限制，我们将国际先进的管理理念和方法，同中国境内企业运作的实际情况相结合，以独特的方法和工具，使深奥、晦涩的专业内容简单化、趣味化，为各类企业提供专业性、实用性强的高品质培训和管理咨询服务。 </p>
+										<p>我们的培训师有来自不同行业国内外知名企业的、具有极强实战经验的高级职业经理人，也有来自各高校、科研机构的专家学者。公司有一整套高效而专业的内外训课程体系、科学的讲师评估机制和培训效果控管机制，为企业量身定做一套紧扣企业实际，切实可行的培训。目前，公司每年召开公开课近200期，定期在沪、京、粤及长江三角洲等地举行；而企业内部培训的足迹遍布全国诸多省市。在近几年的发展中，并以优质的服务赢得客户的高度评价。 </p>
+									</div>
+									<div class="art">
+										<h3>服务方式</h3>
+										<ul>
+											<li>优质公开课：每年为客户提供全国范围内优质公开课</li>
+											<li>顾问式内训：公司提供一百多门具有国际水准的专业顾问课程</li>
+											<li>企业持续发展：每年为十余家企业提供企业持续发展方案</li>
+										</ul>
+									</div>
+									<div class="art">
+										<h3>企业文化</h3>
+										<ul>
+											<li>企业理念：让智力资源以更高效的方式服务客户，帮助企业健康成长</li>
+											<li>聚宇宗旨：传播新理念；学习新知识；捕捉新机遇；创造新思维．</li>
+											<li>聚宇精神：以人为本，创新为魂；态度为首；高效为功．</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+                        </div>
+					</div>
+				</div>
+			</div>
+            <div class="footer">
+                <p>
+                    <a href="/about" target="_blank">关于我们</a> |
+                    <!--<a href="#">广告服务</a> |
+                    <a href="#">免责声明</a> |
+                    <a href="#">网站地图</a> |-->
+                    <a href="/copyright" target="_blank">版权信息</a> |
+                    <a href="/flink" target="_blank">友情链接</a>
+                </p>
+                <p>
+                    Copyright &copy; 2010-2015 上海聚宇培训网
+                </p>
+                <!--<p>
+                沪ICP备:1000000号
+                </p>-->
+            </div>
+        </div>
+<script type="text/javascript">
+function show_step(step){
+	$("dt a")
+		.attr("class","");
+	$("dt a")
+		.eq(step)
+		.attr("class","cerrent");
+	$(".step_part")
+			.hide()
+			.eq(step)
+			.show();
+}
+</script>
